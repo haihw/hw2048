5d09752c4c8774b80489d0363bdf685923b1d2e4
Hai Hw <nguyenhuuhiephai@gmail.com>
2014-03-26 10:34:46 +0800

    Initial Commit

---
+++ game4096/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  game4096
+//
+//  Created by Hai Hw on 26/3/14.
+//  Copyright (c) 2014 Hai Hw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HWAppDelegate class]));
+    }
+}
